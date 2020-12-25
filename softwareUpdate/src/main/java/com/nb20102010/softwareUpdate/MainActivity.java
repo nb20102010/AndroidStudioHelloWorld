be79715beb6c9baa95760960a2ed7f55bfd5d0eb
nb20102010 <lirunliruntc@gmail.com>
2020-12-25 12:29:15 -0800

    p24

---
+++ softwareUpdate/src/main/java/com/nb20102010/softwareUpdate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nb20102010.softwareUpdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
